57f00aea86e07786b0cee48ce7d672e427415575
Augusto Oliveira <soninclovis@gmail.com>
2025-02-13 19:53:39 -0300

    implement error handling

--- index.js
+++ index.js
@@ -8,17 +8,30 @@ const port = process.env.PORT;
 app.use(e.json());
 
 app.get("/tasks", async (req, res) => {
-  const queryString = "SELECT * FROM tasks;";
-  const { rows } = await database.query(queryString);
-  res.status(200).json(rows);
+  try {
+    const queryString = "SELECT * FROM tasks;";
+    const { rows } = await database.query(queryString);
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.post("/tasks", async (req, res) => {
-  const { title, description } = req.body;
-  const queryString =
-    "INSERT INTO tasks (title, description) VALUES($1, $2) RETURNING *;";
-  const { rows } = await database.query(queryString, [title, description]);
-  res.status(200).json(rows);
+  try {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ error: "Title and description are required" });
+    }
+    const queryString =
+      "INSERT INTO tasks (title, description) VALUES($1, $2) RETURNING *;";
+    const { rows } = await database.query(queryString, [title, description]);
+    res.status(201).json(rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
